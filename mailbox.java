4b4b6fe7c80f4eb14fec0fc066d210f3aa1c5ce3
Alexander Eric <70110009+alexandereric995@users.noreply.github.com>
2021-02-15 14:39:29 +0800

    Create mailbox.java

---
+++ mailbox.java
@@ -0,0 +1,75 @@
+/*
+* Edit by Alexander Eric
+ */
+
+
+
+import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+
+/**
+ * This is a subclass of JInternalFrame which displays a tree.
+ *
+ * @author Steve Wilson
+ * @author Alexander Kouznetsov
+ */
+@SuppressWarnings("serial")
+public class MetalworksInBox extends JInternalFrame {
+
+    public MetalworksInBox() {
+        super("In Box", true, true, true, true);
+
+        DefaultMutableTreeNode unread;
+        DefaultMutableTreeNode personal;
+        DefaultMutableTreeNode business;
+        DefaultMutableTreeNode spam;
+
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Mail Boxes");
+
+        top.add(unread = new DefaultMutableTreeNode("Unread Mail"));
+        top.add(personal = new DefaultMutableTreeNode("Personal"));
+        top.add(business = new DefaultMutableTreeNode("Business"));
+        top.add(spam = new DefaultMutableTreeNode("Spam"));
+
+        unread.add(new DefaultMutableTreeNode("Inbox"));
+        unread.add(new DefaultMutableTreeNode("Inbox"));
+        unread.add(new DefaultMutableTreeNode("Inbox"));
+        unread.add(new DefaultMutableTreeNode("Inbox"));
+        unread.add(new DefaultMutableTreeNode("Inbox"));
+
+        personal.add(new DefaultMutableTreeNode("Inbox"));
+        personal.add(new DefaultMutableTreeNode("Inbox"));
+        personal.add(new DefaultMutableTreeNode("Inbox"));
+
+        business.add(new DefaultMutableTreeNode("Inbox"));
+        business.add(new DefaultMutableTreeNode("Inbox"));
+        business.add(new DefaultMutableTreeNode("Inbox"));
+        business.add(new DefaultMutableTreeNode(
+                "Project Metal: delivered on time"));
+        business.add(new DefaultMutableTreeNode("Inbox"));
+
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+        spam.add(new DefaultMutableTreeNode("Inbox"));
+
+        JTree tree = new JTree(top);
+        JScrollPane treeScroller = new JScrollPane(tree);
+        treeScroller.setBackground(tree.getBackground());
+        setContentPane(treeScroller);
+        setSize(325, 200);
+        setLocation(75, 75);
+
+    }
+}
